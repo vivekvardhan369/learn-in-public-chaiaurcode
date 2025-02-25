64ea6ae68977fb45cc5dfe8ccf3ad7e2825d51c5
vivekvardhan369 <vivekvardhan.akula@gmail.com>
2025-02-25 19:12:38 +0530

    [2025-02-25] JavaScript - A mini-project on Date&Time Widget"

---
+++ Session-16/01_project/script.js
@@ -0,0 +1,25 @@
+
+function updateClock(){
+    const timeElement = document.getElementById("time");
+    const dateElement = document.getElementById("date");
+
+    const now = new Date()
+    const hours = now.getHours() % 12 || 12
+    const minutes = now.getMinutes().toString().padStart(2,"0")
+    // const minutes = minutes < 10 ? `0${minutes}` : `${minutes}`
+    const seconds = now.getSeconds().toString().padStart(2,"0")
+    const ampm = now.getHours() >=12 ? "PM" : "AM"
+
+    const options = {
+        weekday : "long",
+        year : "numeric",
+        month : "long",
+        day : "numeric"
+    }
+
+    timeElement.textContent = `${hours}:${minutes}:${seconds}:${ampm}`
+    dateElement.textContent = now.toLocaleDateString(undefined,options)
+
+}
+setInterval(updateClock,1000)
+updateClock()
